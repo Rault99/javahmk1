8b7c4886169140651eaba79bc6149742e1b5c8cd
ryan-ault <ryanault012@gmail.com>
2021-10-09 13:28:30 -0600

    homework push

--- homework.js
+++ homework.js
@@ -15,3 +15,22 @@ var h = 4;
 var f = d / h;
 
 console.log(f);
+
+var r = 7;
+var k = 7;
+var o = r * k;
+
+console.log(o);
+
+var string1 = "hello";
+var string2 = "world";
+var string3 = string1 + " " + string2;
+
+console.log(string3);
+
+var string4 = "my favortie food";
+var string5 = "is fresh fruit";
+var string6 = string4 + " " + string5;
+
+console.log(string6);
+
