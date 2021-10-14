c421eb75286519fa5a0c3e51e0040d31efa4e5ab
ryan-ault <ryanault012@gmail.com>
2021-10-13 19:13:07 -0600

    ifthen

---
+++ forloop.js
@@ -0,0 +1,3 @@
+for(var i = 1000; i > -1000; i = i - 100){
+    console.log(i);
+}
---
+++ ifthen.js
@@ -0,0 +1,67 @@
+var hoursofsleep = 8
+
+if (hoursofsleep<6)
+console.log("I am groggy.");
+ else 
+    console.log ("i feel fantastic!'");
+
+if (5>3){
+    console.log("5 is greater than 3, is true");
+
+}
+
+if (4==3){
+    console.log("4 is not equal to 3, its false");
+
+}
+
+var x = 3; 
+var y = 4; 
+
+if (x>y){
+    console.log("x is greater than y");
+}
+
+
+var x = 3;
+
+var y = 5;
+
+if (x>y){
+    console.log("x is greater than y");
+}else{
+    console.log("running default statement");
+}
+
+var x = 3;
+var y = 5;
+
+var string1 = 'Cat';
+var string2 = 'dog';
+
+if (x<y || string1 != string2){
+    console.log("x is less then y AND cat does not equal dog");
+}
+
+var hoursofsleep = 8;
+
+if (hoursofsleep < 6){
+    console.log("i am groggy.");
+}else{
+    console.log("i feel fantastic?");
+}
+
+
+    var nextlocation = "HOME";
+    if(nextlocation === "HOME"){
+
+    console.log("its been a long day, lets go home!");
+
+}elseif(nextlocation === "work") {
+    console.log("good morning, finding the quickest route to the glue factory!");
+} else {
+
+     console.log("horses not included");
+
+}
+
